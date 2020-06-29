6b1438cc9eaf69a46d35b4919b1c59d084596d6c
Jocelyn Fuentes <jfuentes411@gmail.com>
2020-06-29 18:16:58 +0000

    Removed files in .gitignore

--- my_cred.py
+++
@@ -1,5 +0,0 @@
-
-
-wordpress_connect =  'mysql+pymysql://root:harvey@127.0.0.1:3306/wordpress'
-
-vp_connect =  'mysql+pymysql://root:harvey@127.0.0.1:3306/vinepair1'
--- vp_cred.py
+++
@@ -1,5 +0,0 @@
-
-
-wordpress_connect =  'mysql+pymysql://vinepair:2019Zinfind#l@18.209.226.55:3306/wordpress'
-
-vinepair_connect =  'mysql+pymysql://vinepair:2019Zinfind#l@18.209.226.55:3306/vinepair1'
